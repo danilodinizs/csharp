8d8d8b2df0bbc2e031f2affa9a9e195a2fb0728c
Danilo Diniz <danilodiniz93@yahoo.com>
2023-10-04 17:47:18 -0300

    cap 17 linq w/ lambda

--- first/Interfaces/Program.cs
+++ first/Interfaces/Program.cs
@@ -1,25 +1,24 @@
-﻿using Interfaces.Entities;
-using Interfaces.Services;
-using System.Globalization;
+﻿//using Interfaces.Entities;
+//using Interfaces.Services;
+//using System.Globalization;
 
-CultureInfo ci = CultureInfo.InvariantCulture;
+//CultureInfo ci = CultureInfo.InvariantCulture;
 
-List<Product> list = new List<Product>();
+//List<Product> list = new List<Product>();
 
-Console.Write("Enter the number of products: ");
-int n = int.Parse(Console.ReadLine());
+//Console.Write("Enter the number of products: ");
+//int n = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < n; i++)
-{
-    string[] vect = Console.ReadLine().Split(',');
-    double price = double.Parse(vect[1], ci);
-    list.Add(new Product(vect[0], price));
-}
+//for (int i = 0; i < n; i++)
+//{
+//    string[] vect = Console.ReadLine().Split(',');
+//    double price = double.Parse(vect[1], ci);
+//    list.Add(new Product(vect[0], price));
+//}
 
-CalculationService calculationService = new CalculationService();
+//CalculationService calculationService = new CalculationService();
 
-Console.WriteLine("Most expensive: ");
-Console.WriteLine(calculationService.Max(list));
-
+//Console.WriteLine("Most expensive: ");
+//Console.WriteLine(calculationService.Max(list));
 
 
