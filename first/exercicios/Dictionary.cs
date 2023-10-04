0628756e95c656fb3b6395252e90432b407c9ea2
Danilo Diniz <danilodiniz93@yahoo.com>
2023-10-03 23:46:28 -0300

    cap 16

---
+++ first/exercicios/Dictionary.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+
+    using (StreamReader sr = File.OpenText(path))
+    {
+
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(',');
+            string name = line[0];
+            int votos = int.Parse(line[1]);
+
+            if (dictionary.ContainsKey(name))
+            {
+                dictionary[name] += votos;
+            }
+            else
+            {
+                dictionary[name] = votos;
+            }
+        }
+
+
+
+
+        foreach (KeyValuePair<string, int> kvp in dictionary)
+        {
+            Console.WriteLine(kvp.Key + ": " + kvp.Value);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("Any error ocurred!");
+    Console.WriteLine(e.Message);
+}
